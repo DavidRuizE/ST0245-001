d4488119727138f466a2deb3cdc697250d30115b
Tomasgaviria <73371810+Tomasgaviria@users.noreply.github.com>
2021-01-29 11:14:08 -0500

    Add files via upload

---
+++ talleres/taller01/Punto.java
@@ -0,0 +1,29 @@
+public class Punto {
+
+    private double x, y;
+
+    public Punto(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+  
+    public double x() {
+        return x;
+    }
+
+    public double y() {
+        return y;
+    }
+
+    public double radioPolar() {
+        return Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
+    }
+
+    public double anguloPolar() {
+        return Math.atan(y/x);
+    }
+    
+      public double distanciaEuclidiana(Punto otro) {
+        return Math.sqrt(Math.pow(otro.x() - x,2) + Math.pow(otro.y() - y,2));
+    }
+}
