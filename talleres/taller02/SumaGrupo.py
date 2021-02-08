997b6ad08981c683caa53ab0ce346a59bc3f1f27
Tomasgaviria <73371810+Tomasgaviria@users.noreply.github.com>
2021-02-08 13:03:13 -0500

    Update SumaGrupo.py

--- talleres/taller02/SumaGrupo.py
+++ talleres/taller02/SumaGrupo.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 def SumaGrupo(start, nums, target):
     if (start == len(nums)):
         return target == 0
@@ -11,4 +5,3 @@ def SumaGrupo(start, nums, target):
         usar = Suma_grupo(start+1, nums, target-nums[start])
         noUsar = Suma_grupo(start+1, nums, target)
         return usar or noUsar
-
