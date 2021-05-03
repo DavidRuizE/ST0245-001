6598eece72f700c76cf1a035f25ec88263c272a9
Tomasgaviria <73371810+Tomasgaviria@users.noreply.github.com>
2021-05-02 22:44:37 -0500

    Add files via upload

---
+++ laboratorios/lab04/codigo/Bee.java
@@ -0,0 +1,23 @@
+public class Bee
+{
+    private double latitude;
+    private double longitude;
+    private double altitude;
+    public Bee (double latitude, double longitude, double altitude){
+        this.latitude=latitude;
+        this.longitude=longitude;
+        this.altitude=altitude;
+    }
+    
+    public double getLatitude(){
+        return latitude;
+    }
+    
+    public double getLongitude(){
+        return longitude;
+    }
+    
+    public double getAltitude(){
+        return altitude;
+    }
+}
